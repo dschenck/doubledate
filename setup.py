4c5749d6225b6b328a2df227704c6ebcf8c6245b
David Schenck <david.schenck@outlook.com>
2020-03-01 11:10:01 +0000

    versioning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="doubledate",
-    version="0.0.2",
+    version="0.0.3",
     author="david.schenck@outlook.com",
     author_email="david.schenck@outlook.com",
     description="A calendar wrapper",
