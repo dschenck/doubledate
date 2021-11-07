76431a97747cf75df893f3d2f14d67ba9fff9dad
David Schenck <david.schenck@outlook.com>
2021-11-07 09:47:26 +0100

    change docs theme to furo

--- docs/conf.py
+++ docs/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'doubledate'
-copyright = '2020, David Schenck'
-author = 'David Schenck'
+project = "doubledate"
+copyright = "2021, David Schenck"
+author = "David Schenck"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.5'
+# release = "0.0.5"
 
 
 # -- General configuration ---------------------------------------------------
@@ -30,18 +31,15 @@ release = '0.0.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    'sphinx.ext.napoleon'
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_copybutton"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,15 +47,18 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-#html_theme = 'classic'
-html_theme = 'nature'
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+
+
+html_logo = "_static/logo.svg"
+
+html_theme_options = {"sidebar_hide_name": False}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-#https://github.com/readthedocs/readthedocs.org/issues/2569
-master_doc = 'index'
+# https://github.com/readthedocs/readthedocs.org/issues/2569
+master_doc = "index"
+
