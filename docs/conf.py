eee88e48f378adc1eb6e65dfa0a27f6f4b231158
David Schenck <david.schenck@outlook.com>
2021-12-05 18:44:41 +0100

    [docs][bugfix] Remove extensions

--- docs/conf.py
+++ docs/conf.py
@@ -31,13 +31,7 @@ author = "David Schenck"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx_copybutton",
-    "sphinx.ext.autosummary",
-    "sphinx_automodapi.automodapi",
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "sphinx_copybutton"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
