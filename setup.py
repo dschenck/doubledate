98eae2c1ce0374e167e534d1ee4f31d56d9e22ff
David Schenck <david.schenck@outlook.com>
2026-03-08 22:43:33 +0100

    fix: ensure setup.py version is read from __version__ in __init__.py (#10)

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
 import setuptools
+import re
+import ast
+import os
+
+# https://stackoverflow.com/questions/6786555/automatic-version-number-both-in-setup-py-setuptools-and-source-code
+with open(os.path.join(os.path.dirname(__file__), "doubledate/__init__.py"), "rb") as f:
+    version = str(
+        ast.literal_eval(
+            re.compile(r"__version__\s+=\s+(.*)")
+            .search(f.read().decode("utf-8"))
+            .group(1)
+        )
+    )
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="doubledate",
-    version="0.0.14",
+    version=version,
     author="david.schenck@outlook.com",
     author_email="david.schenck@outlook.com",
     description="A calendar wrapper",
